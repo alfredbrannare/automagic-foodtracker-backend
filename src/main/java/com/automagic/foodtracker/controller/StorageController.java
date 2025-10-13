1b7185c479a3a1f6ee79888a31c309d99d95b4e2
Alfred Brännare <alfred-brannare@hotmail.com>
2025-10-13 15:44:38 +0200

    wip: add initial StorageController

--- src/main/java/com/automagic/foodtracker/controller/StorageController.java
+++ src/main/java/com/automagic/foodtracker/controller/StorageController.java
@@ -1,12 +1,18 @@
 package com.automagic.foodtracker.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class StorageController {
-    @GetMapping("/storage")
-    public String getStorages() {
-        return "Hello Affe";
+    @GetMapping("/api/storage")
+    public String getStorages(Principal principal) {
+
+        return "Hello Storage";
     }
 }
+
+//Principal principal - Injectas som dependency
